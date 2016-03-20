ef82880ad39fd2da453abe5bb36018e390457444
Reelos Hericon <reelos.hericon@web.de>
2016-03-20 12:41:15 +0100

    failed on first try

---
+++ src/main/java/de/reelos/wordfilter/WordFilter.java
@@ -0,0 +1,15 @@
+package de.reelos.wordfilter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WordFilter extends JavaPlugin{
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
